a57c9083a139260fb2ed8c33389cf65e28b9fbd2
Wevison Ramalho Silva <wevisonramalho@gmail.com>
2022-08-26 15:17:00 -0300

    [ FEATURE ] Creating component Lesson.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,34 @@
+import { gql, useQuery } from "@apollo/client";
+
 import { Lesson } from "./Lesson";
 
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      lessonType
+      availableAt
+      title
+      slug
+    }
+  }
+`;
+
+type Lesson = {
+  id: string;
+  title: string;
+  slug: string;
+  availableAt: string;
+  lessonType: "live" | "class";
+};
+
+interface GetLessonsQueryResponse {
+  lessons: Lesson[];
+}
+
 export function Sidebar() {
+  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
+
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
       <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
@@ -8,12 +36,15 @@ export function Sidebar() {
       </span>
 
       <div className="flex flex-col gap-8">
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
+        {data?.lessons.map((lesson: Lesson) => (
+          <Lesson
+            key={lesson.id}
+            title={lesson.title}
+            slug={lesson.slug}
+            avaliableAt={new Date(lesson.availableAt)}
+            type={lesson.lessonType}
+          />
+        ))}
       </div>
     </aside>
   );
